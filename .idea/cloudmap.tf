187e7b72b198a627cc969eca3951820f5d07366f
나연 <kny5579@naver.com>
2025-04-25 19:31:38 +0900

    feat(#1): 라우팅에 필요한 cloudmap 파일추가

---
+++ .idea/cloudmap.tf
@@ -0,0 +1,24 @@
+resource "aws_service_discovery_private_dns_namespace" "namespace" {
+  name = "couponmoa.local"
+  vpc  = aws_vpc.vpc.id
+  description = "Private namespace for service discovery"
+}
+
+resource "aws_service_discovery_service" "services" {
+  for_each = toset(var.msa_services)
+
+  name = "${var.APP_NAME}-${var.Environment}-${each.key}-service"
+
+  dns_config {
+    namespace_id = aws_service_discovery_private_dns_namespace.namespace.id
+    dns_records {
+      type = "A"
+      ttl  = 10
+    }
+    routing_policy = "MULTIVALUE"
+  }
+
+  health_check_custom_config {
+    failure_threshold = 1
+  }
+}
